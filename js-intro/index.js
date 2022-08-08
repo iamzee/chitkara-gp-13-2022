5f52f141e13e5fbac64b23ed18a98eed4d85a01f
Zeeshan Adeen <zeeshan99adeen@gmail.com>
2022-08-08 10:17:40 +0530

    js-intro

---
+++ js-intro/index.js
@@ -0,0 +1,46 @@
+// let email = 'abc@xyz.com';
+// console.log(email);
+
+// let a1b3c = 'dsjhdsjk';
+
+// let $anything = '';
+// let dh_fjhkdf = 'kuchbhi';
+
+// // let firstname = 'Zeeshan';
+// // console.log(firstname);
+
+
+// let firstname = 'Zeeshan'; // normal
+// let firstName = 'Zeeshan'; // camel case
+// let first_name = 'Zeeshan'; // snake case
+// // let first-name = 'Zeeshan'; // kebab case
+
+// let lastName = 'Adeen';
+// console.log(lastName);
+
+// let firstName = 'Zeeshan';
+// let lastName = 'Adeen';
+
+// let fullName = firstName + lastName;
+
+// console.log(fullName);
+
+// I live in Delhi, India
+// let city = 'Delhi';
+// let country = 'India';
+
+// let description = 'I live in ' + city + ', ' + country;
+// console.log(description);
+
+// 3 ways to declare strings
+// let password = 'hello';
+// let anotherPass = "h";
+// let oneMorePassword = `abc`;
+
+let fullName = 'Zeeshan\nAdeen';
+console.log(fullName);
+
+//It's here
+
+let desc = 'I\'s here';
+console.log(desc);
