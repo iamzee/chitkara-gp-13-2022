24dade2d8e745784f0f58dc46d829e4541f3875f
Zeeshan Adeen <zeeshan99adeen@gmail.com>
2022-09-05 10:58:08 +0530

    lect-5 and 6

--- js-lect-5/index.js
+++ js-lect-5/index.js
@@ -16,3 +16,88 @@ let arr = [1, 2, 3, 4];
 arr.push(5);
 
 console.log(arr);
+
+// ----------------------------
+// constructor functions
+
+function user(firstName) {
+  this.username = firstName;
+  this.bio = function() {
+    console.log('My name is something');
+  }
+}
+
+const user1 = new user('Messi');
+console.log(user1);
+const user2 = new user('Ronaldo');
+console.log(user2);
+
+/*
+ TODO constructor function
+ title -> string
+ completed -> boolean
+ markCompleted -> completed = true
+*/
+
+// function Todo(title, completed) {
+//   this.title = title;
+//   this.completed = completed;
+//   this.markCompleted = function() {
+//     this.completed = true;
+//   }
+// }
+
+class Todo {
+  constructor(title, completed) {
+    this.title = title;
+    this.completed = completed;
+    this.markCompleted = function() {
+      this.completed = true;
+    }
+  }
+}
+class StudentTodo extends Todo {
+  constructor(title, completed, rollNo) {
+    super(title, completed);
+    this.rollNo = rollNo;
+  }
+}
+
+const student = new StudentTodo('Buy grioceries', false, 20);
+/*
+{
+  title: 'Buy groceries',
+  completed: false
+}
+*/
+
+// --------------------------------
+// ARROW FUNCTIONS
+
+function greeting1() {
+  console.log('Hello world1');
+}
+greeting1();
+
+let greeting2 = function() {
+  console.log('Hello world2')
+}
+greeting2();
+
+// ARROW FUNCITON
+let greeting3 = () => {
+  console.log('Hello world3');
+}
+
+greeting3();
+
+let sum = (num1, num2) => {
+  return num1 + num2;
+}
+
+let sum2 = (num1, num2) =>  num1 + num2;
+
+console.log(sum(2, 3));
+
+
+
---
+++ js-lect-6/index.js
@@ -0,0 +1,15 @@
+
+
+
+
+console.log('one');
+
+setTimeout(function(){
+  console.log('hello world 1');
+}, 4000);
+
+setTimeout(function() {
+  console.log('hello world 2')
+}, 2000);
+
+console.log('two');
