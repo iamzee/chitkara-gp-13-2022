98029c6728505f684befd00afc63a3dc5cbf9e73
Zeeshan Adeen <zeeshan99adeen@gmail.com>
2022-10-03 10:03:13 +0530

    js-modules

---
+++ modules/index1.js
@@ -0,0 +1,6 @@
+export let firstName = 'Coding';
+export let lastName = 'Blocks';
+
+export default function sum(num1, num2) {
+  console.log(num1 + num2);
+}
---
+++ modules/index2.js
@@ -0,0 +1,7 @@
+import mySum, { firstName, lastName} from './index1.js';
+// import { lastName } from './index1.js';
+
+console.log(firstName);
+console.log(lastName);
+
+mySum(40, 50);
